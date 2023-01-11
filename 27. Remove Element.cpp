afe3f71f9c6533ced84da45c212647b20729b72e
EuL <101369119+ETjoel@users.noreply.github.com>
2023-01-11 18:46:37 +0300

    12 minutes

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.size()-count; i++){
+            if(nums[i] == val){
+                for(int j = i; j< nums.size() - 1; j++){
+                    nums[j] = nums[j+1];
+                }
+                count++;i--;
+            }
+        }
+        return nums.size() - count;
+    }
+};
